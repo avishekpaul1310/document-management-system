b0c4bfbe6b8f946b106ce5c0ff6944fc4ec24b55
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-01-29 18:25:27 +0530

    commit

--- documents/forms.py
+++ documents/forms.py
@@ -5,3 +5,6 @@ class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
         fields = ['title', 'description', 'file', 'category', 'is_shared']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
