4fc98afe88eb669a4a076c5d84408b7ff397a634
avishekpaul1310 <avishekpaul1310@gmail.com>
2025-01-29 18:40:24 +0530

    commit

--- docsystem/settings.py
+++ docsystem/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'docsystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Add this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
